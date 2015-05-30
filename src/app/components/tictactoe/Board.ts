97aa090ac8982fe71ba0257e911ce7d6ed236866
Sekib Omazic <sekib.omazic@gmail.com>
2015-05-30 11:29:51 +0200

    tic tac toe simplified. Tile component removed

--- src/app/components/tictactoe/Board.ts
+++ src/app/components/tictactoe/Board.ts
@@ -4,8 +4,6 @@
 // Angular 2
 import { Component, View, NgFor, Observable, EventEmitter } from 'angular2/angular2';
 
-import { Tile } from './tile';
-
 @Component({
   selector: 'board',
   properties: {
@@ -14,14 +12,16 @@ import { Tile } from './tile';
   events: ['select']
 })
 @View({
-  directives: [NgFor, Tile],
+  directives: [ NgFor ],
   template:`
     <div class="board">
       <div *ng-for="#row of board; #x=index" class="row">
         <div *ng-for="#tile of row; #y=index">
-          <tile [model]="tile"
-                (^click)="play(x, y)">
-          </tile>
+          <div class="tile"
+               [class.x]="tile=='x'"
+               [class.o]="tile=='o'"
+               (^click)="play(x, y)">
+          </div>
         </div>
       </div>
     </div>
